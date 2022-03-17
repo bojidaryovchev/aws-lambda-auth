fe244c637f37815985dff1538f7ba4299fa3b1c6
bojidaryovchev <blizzardcon10@gmail.com>
2022-03-17 05:29:52 +0200

    feat: DynamoDB access

--- amplify/backend/function/auth/src/index.js
+++ amplify/backend/function/auth/src/index.js
@@ -1,8 +1,20 @@
 const process = require('process');
+const AWS = require('aws-sdk');
+const uuid = require('uuid');
 
-// 
+const dynamo = new AWS.DynamoDB.DocumentClient();
 
 exports.handler = async (event) => {
+  await dynamo
+    .put({
+      TableName: 'users',
+      Item: {
+        id: uuid.v4(),
+        email: 'email@provider.ext',
+      },
+    })
+    .promise();
+
   // TODO implement
   const response = {
     statusCode: 200,
@@ -14,6 +26,7 @@ exports.handler = async (event) => {
     body: JSON.stringify({
       event: JSON.stringify(event),
       jwtSecret: process.env.JWT_SECRET ? process.env.JWT_SECRET.length : 0,
+      something: 'Dark Side',
     }),
   };
   return response;
